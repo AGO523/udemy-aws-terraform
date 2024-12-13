aed132ed67510f1142720a14afd1bcb738ed3a81
AGO523 <unpouletfrais0523@gmail.com>
2024-12-13 09:08:50 +0900

    add: s3 static bucket

---
+++ s3.tf
@@ -0,0 +1,48 @@
+resource "random_string" "s3_unique_key" {
+  length  = 6
+  upper   = false
+  lower   = true
+  special = false
+}
+
+# -----------------------
+# state bucket
+# -----------------------
+resource "aws_s3_bucket" "s3_static_bucket" {
+  bucket = "${var.project}-${var.environment}-static-bucket-${random_string.s3_unique_key.result}"
+}
+
+resource "aws_s3_bucket_versioning" "s3_static_bucket_versioning" {
+  bucket = aws_s3_bucket.s3_static_bucket.id
+
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_static_bucket_block" {
+  bucket                  = aws_s3_bucket.s3_static_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+
+  depends_on = [aws_s3_bucket_policy.s3_static_bucket_policy]
+}
+
+resource "aws_s3_bucket_policy" "s3_static_bucket_policy" {
+  bucket = aws_s3_bucket.s3_static_bucket.id
+  policy = data.aws_iam_policy_document.s3_static_bucket_document.json
+}
+
+data "aws_iam_policy_document" "s3_static_bucket_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.s3_static_bucket.arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_iam_role.app_iam_role.arn}"]
+    }
+  }
+}
